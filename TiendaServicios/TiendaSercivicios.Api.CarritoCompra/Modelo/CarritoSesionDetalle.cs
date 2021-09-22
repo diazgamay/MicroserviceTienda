9b4ae918fd5fa3ba153f26b9028c3020d0dcd62f
diazgamay <64745792+diazgamay@users.noreply.github.com>
2021-09-21 21:05:37 -0500

    New Api

---
+++ TiendaServicios/TiendaSercivicios.Api.CarritoCompra/Modelo/CarritoSesionDetalle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiendaServicios.Api.CarritoCompra.Modelo
+{
+    public class CarritoSesionDetalle
+    {
+        public int CarritoSesionDetalleId { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+        public string ProductoSelecciondo { get; set; }
+        public int CarritoSesionId { get; set; }//Clave foranea
+        public CarritoSesion CarritoSesion { get; set; }//Ancla para unir CarritoSesion y CarritoSesionDetalle
+    }
+}
---
+++ TiendaServicios/TiendaServicios.Api.Autor/Aplicacion/Nuevo.cs
@@ -0,0 +1,66 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TiendaServicios.Api.Autor.Modelo;
+using TiendaServicios.Api.Autor.Persistencia;
+
+namespace TiendaServicios.Api.Autor.Aplicacion
+{
+    public class Nuevo
+    {
+        public class Ejecuta : IRequest
+        {
+            public string Nombre { get; set; }
+            public string Apellido { get; set; }
+            public DateTime FechaNacimiento { get; set; }
+        }
+
+        public class EjecutaValidation:AbstractValidator<Ejecuta>
+        {
+            public EjecutaValidation()
+            {
+                RuleFor(x => x.Nombre).NotEmpty();
+                RuleFor(x => x.Apellido).NotEmpty();
+            }
+        }
+
+        public class Manejador:IRequestHandler<Ejecuta>
+        {
+            public readonly ContextoAutor _contexto;
+
+            public Manejador(ContextoAutor contexto)
+            {
+                _contexto = contexto;
+            }
+
+            public async Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
+            {
+                var autorLibro = new AutorLibro
+                {
+                    Nombres = request.Nombre,
+                    Apellido = request.Apellido,
+                    FechaNacimiento = request.FechaNacimiento,
+                    AutorLibroGuid = Convert.ToString(Guid.NewGuid())
+                };
+
+                _contexto.AutorLibros.Add(autorLibro);
+               var valor= await _contexto.SaveChangesAsync();
+
+               if(valor>0)
+                {
+                    return Unit.Value;
+                }
+
+                throw new Exception("No se puede insertar el Autor del libro.");
+
+
+            }
+
+        }
+
+    }
+}
